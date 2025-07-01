cb9fbbaea9889610ef94ac14d88d307e96f1ca9c
patilabhi45 <abhishek.patil@webknot.in>
2025-07-01 16:44:02 +0530

    ABHISHEK | ecommerse springboot application

---
+++ ecommerce-inventory-management/src/test/java/com/example/ecommerceinventory/EcommerceInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
